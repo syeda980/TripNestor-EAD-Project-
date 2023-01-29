77803ef3cfefc8df0e3741456b06a89402eb409f
syedaLaiba <laiba033545@gmail.com>
2023-01-29 16:27:56 +0500

    updated with ef code and updated wallpaper picture

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,11 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TripNestor.Models;
 
 namespace TripNestor.Controllers
 {
     public class UserController : Controller
     {
+        TripNestorContext tripNestorContext = new TripNestorContext();
         public ViewResult Signup()
         {
             return View();
@@ -16,5 +18,51 @@ namespace TripNestor.Controllers
         {
             return View("signin");
         }
+        [HttpPost]
+        public ViewResult AddUserToDB(string userName,string userEmail,string userPassword)
+        {
+            User newUser = new User();
+            newUser.Name = userName;
+            newUser.Email = userEmail;
+            newUser.Password = userPassword;
+            tripNestorContext.Users.Add(newUser);
+            int enteries=tripNestorContext.SaveChanges();
+            if (enteries > 0)
+            {
+                ViewBag.Message = "Account has been successfully Registered!!!";
+                ViewBag.flag = true;
+            }
+            else
+            {
+                ViewBag.Message = "Sorry Account can't be registered at the moment... Please try Again";
+                ViewBag.flag = false;
+            }
+            return View("signin");
+        }
+
+        [HttpPost]
+        public ViewResult Login(User user)
+        {
+            var userExist = tripNestorContext.Users.FirstOrDefault(u => u.Email== user.Email && u.Password == user.Password);
+
+            if(userExist!=null)
+            {
+                //cookies management 
+                HttpContext.Response.Cookies.Append("userEmail",user.Email);
+                HttpContext.Response.Cookies.Append("userPassword", user.Password);
+                return View("userHome");
+            }
+            else
+            {
+                ViewBag.invalidmsg = "Invalid Username or password";
+                ViewBag.flag = false;
+                ViewBag.invalid = true;
+                //ModelState.Clear();//clear the fields
+                return View("signin");
+
+            }
+
+        }
+
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,6 +9,7 @@ namespace TripNestor.Models
     public partial class User
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
