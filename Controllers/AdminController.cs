36de696d13948b5a4ba002f35d8da8c769100750
syedaLaiba <laiba033545@gmail.com>
2023-01-29 22:39:33 +0500

    updated code

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using TripNestor.Models;
 
@@ -18,28 +19,45 @@ namespace TripNestor.Controllers
         {
             return View();
         }
-        public ViewResult Login(Admin admin)
+        public ActionResult Login(Admin admin)
         {
             var userExist = tripNestorContext.Admins.FirstOrDefault(u => u.Email == admin.Email && u.Password == admin.Password);
 
             if (userExist != null)
             {
                 //cookies management 
-                HttpContext.Response.Cookies.Append("userEmail", admin.Email);
-                HttpContext.Response.Cookies.Append("userPassword", admin.Password);
-                return View("adminHome");
+                HttpContext.Response.Cookies.Append("adminEmail", admin.Email);
+                HttpContext.Response.Cookies.Append("adminPassword", admin.Password);
+                return RedirectToAction("adminHome","Admin");
             }
             else
             {
                 ViewBag.invalidmsg = "Invalid Email or password";
                 ViewBag.invalid = true;
-                //ModelState.Clear();//clear the fields
                 return View("Signin");
-
             }
+        }
+        public ViewResult adminHome()
+        {
+            if(HttpContext.Request.Cookies.ContainsKey("adminEmail")
+                && HttpContext.Request.Cookies.ContainsKey("adminPassword"))
+            {
+                return View("adminHome");
+            }
+            else
+            {
+                ViewBag.invalidmsg = "Please Log in first to proceed further";
+                ViewBag.invalid = true;
+                return View("Signin");
+            }
+        }
+        public ViewResult Logout()
+        {
+            HttpContext.Response.Cookies.Delete("adminEmail");
+            HttpContext.Response.Cookies.Delete("adminPassword");
+            return View("Signin");
 
 
         }
-
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,6 +36,7 @@ namespace TripNestor.Controllers
                 return View();
             }
 ;        }
+        
 
     }
 }
